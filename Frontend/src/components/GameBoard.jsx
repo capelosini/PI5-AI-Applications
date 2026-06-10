6c9a236f4bae2fc47940a7de9a3d1abe446b9d76
Clóvis A Pelosini <capelosini@gmail.com>
2026-06-10 19:01:42 -0300

    Add colors depending on the player token team

--- Frontend/src/components/GameBoard.jsx
+++ Frontend/src/components/GameBoard.jsx
@@ -1,26 +1,34 @@
 export default function GameBoard({ board }) {
-    if (!board) return null;
+  if (!board) return null;
 
-    return (
-        <div className="game-board">
-            {board.map((row, rowIndex) => (
-                <div key={rowIndex} className="board-row">
-                    {row.map((cell, colIndex) => (
-                        <div 
-                            key={`${rowIndex}-${colIndex}`} 
-                            className={`board-cell level-${cell.level}`}
-                        >
-                            {cell.level > 0 && <span className="cell-level">{cell.level}</span>}
-                            {cell.professor && (
-                                <div className="professor-token">
-                                    {cell.professor.substring(0, 1)}
-                                    <span className="professor-tooltip">{cell.professor}</span>
-                                </div>
-                            )}
-                        </div>
-                    ))}
+  return (
+    <div className="game-board">
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {row.map((cell, colIndex) => (
+            <div
+              key={`${rowIndex}-${colIndex}`}
+              className={`board-cell level-${cell.level}`}
+            >
+              {cell.level > 0 && (
+                <span className="cell-level">{cell.level}</span>
+              )}
+              {cell.professor && (
+                <div
+                  className={
+                    cell.professor == "CLARO" || cell.professor == "REY"
+                      ? "professor-token professor-token-blue"
+                      : "professor-token professor-token-red"
+                  }
+                >
+                  {cell.professor.substring(0, 1)}
+                  <span className="professor-tooltip">{cell.professor}</span>
                 </div>
-            ))}
+              )}
+            </div>
+          ))}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
